dc8dae8f5df820591d91bc2e67db0f5db7522204
Arpit Madhan <arpit.madhan@gmail.com>
2024-09-07 13:50:36 +0530

    decimal to binary

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number to convert to decimal: ";
+    cin>>n;
+    int bit,ans=0,i=0; 
+    while (n!=0){
+        bit = n&1;
+        ans=((bit* pow(10,i))+ans);
+        n=n>>1;
+        ++i;
+        //cout<<ans<<endl;
+    }
+    cout<<ans;
+}
