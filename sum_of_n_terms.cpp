16b3417876c2bf2f808674feb676725b09e3ff7c
Arpit Madhan <arpit.madhan@gmail.com>
2024-09-07 11:40:02 +0530

    Basic method of summation

---
+++ sum_of_n_terms.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of terms for summation: ";
+    cin>>n;
+    cout<<"Sum of first "<<n<<" natural numbers= "<<n*(n+1)/2<<endl;
+    return 0;
+}
