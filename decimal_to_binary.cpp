9cff089bd67ca6e7a079d7367711b845b10f6783
Arpit Madhan <arpit.madhan@gmail.com>
2024-09-08 09:36:31 +0530

    Learnt c++

--- decimal_to_binary.cpp
+++ decimal_to_binary.cpp
@@ -6,13 +6,13 @@ int main(){
     int n;
     cout<<"Enter number to convert to binary: ";
     cin>>n;
-    int bit,ans=0,i=0; 
+    int bit,i=0;
+    float ans=0; 
     while (n!=0){
         bit = n&1;
         ans=((bit* pow(10,i))+ans);
         n=n>>1;
-        ++i;
-        //cout<<ans<<endl;
+        i++;
     }
     cout<<ans;
 }
