dcf5e0bb6ebec2a817366508fe5f9d8078d497a2
Arpit Madhan <arpit.madhan@gmail.com>
2024-10-11 17:08:14 +0530

    Learnt Array

--- swap_alternate.cpp
+++ swap_alternate.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
-
+void inputarr (int arr[],int size){
+    for (int i=0;i<size;i++){
+        cout<<"enter number: ";
+        cin>>arr[i];
+    }
+}
 void swapit(int &a,int &b){
     int temp;
     temp=a;
@@ -13,9 +18,18 @@ void printarr(int arr[],int n){
     }
 }
 int main(){
-    int num[7]={1,2,3,4,5,6,7};
+    int num[100];
+    int size;
+    cout<<"enter size: ";
+    cin>>size;
+    inputarr(num,size);
+    cout<<endl;
+    cout<<"Original Array: "<<endl;
+    printarr(num,size);
+    cout<<endl;
     for (int i=0;i<6;i=i+2){
         swapit(num[i],num[i+1]);
     }
-    printarr(num,7);
+    cout<<endl<<"Array after alternate swapping: "<<endl;
+    printarr(num,size);
 }
